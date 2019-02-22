ddd0c6354517e7ccc2c8f18b2825c96a60a48018
occmee <occmee@gmail.com>
2019-02-22 14:39:51 +0900

    Clean

--- client/src/components/CreateBook.js
+++ client/src/components/CreateBook.js
@@ -13,9 +13,8 @@ const ADD_BOOK = gql`
   }
 `
 
-
 export default class CreateBook extends Component {
-
+  
   render() {
     return (
       <Mutation mutation={ADD_BOOK} onCompleted={() => window.location.href = "/"}>
@@ -93,78 +92,3 @@ export default class CreateBook extends Component {
     );
   }
 }
-
-// export default () => (
-//   <Mutation mutation={ADD_BOOK} onCompleted={() => window.location.href = "/"}>
-//     {(addBook, { data, loading, error }) => (
-//       <div>
-//         <div className='w-100 pa4 flex justify-center'>
-//           <form
-//             onSubmit={e => {
-//               e.preventDefault();
-//               addBook({
-//                 variables: {
-//                   title: this.title.value,
-//                   cover_image_url: this.coverImage.value,
-//                   average_rating: parseFloat(this.rating.value),
-//                   authorId: parseInt(this.authorId.value)
-//                 }
-//               });
-
-//               this.title.value = "";
-//               this.coverImage.value = "";
-//               this.rating.value = "";
-//               this.authorId.value = "";
-//             }}
-//           >
-
-//             <div style={{ maxWidth: 400 }} className=''>
-//               <label> Book Title: </label>
-//               <input
-//                 className='w-100 pa3 mv2'
-//                 type="text"
-//                 required
-//                 placeholder='Title of the book'
-//                 // ref={node => this.title = node} 
-//                 />
-
-//               <label> Book Cover Image: </label>
-//               <input
-//                 className='w-100 pa3 mv2'
-//                 type="url"
-//                 required
-//                 placeholder='Image Url'
-//                 // ref={node => this.coverImage = node} 
-//                 />
-
-//               <label> Book Rating as decided by Popular votes: </label>
-//               <input
-//                 className='w-100 pa3 mv2'
-//                 type="number"
-//                 required
-//                 min="1"
-//                 max="10"
-//                 placeholder='Average Rating'
-//                 // ref={node => this.rating = node} 
-//                 />
-
-//               <label> Author: </label>
-//               <select
-//                 // ref={select => this.authorId = select}
-//                 name="authorId" required>
-//                 <option value="">Select an author</option>
-//                 <option value="1">Wole Soyinka</option>
-//                 <option value="2">Tomi Adeyemi</option>
-//                 <option value="3">Chimamanda Adichie</option>
-//               </select>
-//             </div>
-//             {loading && <p>Loading...</p>}
-//             {error && <p>Error :( Please try again</p>}
-
-//             <button type="submit">Add Book</button>
-//           </form>
-//         </div>
-//       </div>
-//     )}
-//   </Mutation>
-// );
--- client/src/components/ListBook.js
+++ client/src/components/ListBook.js
@@ -15,7 +15,7 @@ const LIST_BOOKS = gql`
 `
 
 export default class ListBook extends Component {
-
+  
   render() {
     return (
       <Query query={LIST_BOOKS}>
@@ -54,39 +54,3 @@ export default class ListBook extends Component {
     );
   }
 }
-
-// export default () => (
-//   <Query query={LIST_BOOKS}>
-//     {({ loading, error, data }) => {
-//       if (loading) return <p>Loading...</p>;
-//       if (error) return <p>Error...</p>;
-
-//       return (
-//         <div className="col-sm-12">
-//           {!loading &&
-//             data.books.map(book => (
-//               <div className="col-sm-4" key={book.id}>
-//                 <div className='pa3 bg-black-05 ma3'>
-//                   <div
-
-//                     style={{
-//                       backgroundImage: `url(${book.cover_image_url})`,
-//                       backgroundSize: 'cover',
-//                       paddingBottom: '100%',
-//                     }}
-
-//                   />
-//                   <div>
-//                     <div className='book'>
-//                       <h3 align="center"> {book.title}&nbsp; </h3>
-//                       <h4 align="center">Average Rating:  {book.average_rating} / 10 </h4>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </div>
-//             ))}
-//         </div>
-//       );
-//     }}
-//   </Query>
-// );
