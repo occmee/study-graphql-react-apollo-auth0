8c16d767d442fbff39d703b1c9e0562759f170c5
occmee <occmee@gmail.com>
2019-02-22 14:15:34 +0900

    Init client

---
+++ client/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App";
+import { BrowserRouter } from 'react-router-dom';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+import 'tachyons';
+import './index.css';
+
+import { register } from './serviceWorker';
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+register();
